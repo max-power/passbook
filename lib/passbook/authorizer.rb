1572d270954ce188c9d247319ad1db7919771b1f
Kevin <kevin.melchert@gmail.com>
2014-02-27 22:23:19 +0100

    cleanup authorizer

--- lib/passbook/authorizer.rb
+++ lib/passbook/authorizer.rb
@@ -3,21 +3,23 @@ require "base64"
 
 module Passbook
   module Authorizer
-    
     module_function
     
     def sign(content)
-      Base64.decode64 extract(p7(content))
+      Base64.decode64 extract(p7_smime(p7_sign(content)))
     end
     
     def extract(data)
-      start, finish = %{filename="smime.p7s"\n\n}, "\n\n------"
+      start, finish = %[filename="smime.p7s"\n\n], %[\n\n------]
       data[(data.index(start) + start.length)...(data.rindex(finish) + finish.length)]
     end
     
-    def p7(content)
-      data = OpenSSL::PKCS7.sign(p12.certificate, p12.key, content, [wwdr], flags)
-      OpenSSL::PKCS7.write_smime data
+    def p7_smime(data)
+       OpenSSL::PKCS7.write_smime data
+    end
+    
+    def p7_sign(content)
+      OpenSSL::PKCS7.sign(p12.certificate, p12.key, content, [wwdr], flag)
     end
     
     def p12
@@ -28,9 +30,8 @@ module Passbook
       OpenSSL::X509::Certificate.new(Passbook.wwdr_certificate)
     end
     
-    def flags
+    def flag
       OpenSSL::PKCS7::BINARY | OpenSSL::PKCS7::DETACHED
     end
-    
   end
 end
