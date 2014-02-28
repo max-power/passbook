989b0a4c3b386867d98ef092daa310034776f5f6
Max Power <kevin.melchert@gmail.com>
2014-02-28 04:16:13 +0100

    to_der !!!

--- lib/passbook/authorizer.rb
+++ lib/passbook/authorizer.rb
@@ -9,14 +9,7 @@ module Passbook
       wwdr = OpenSSL::X509::Certificate.new(Passbook.wwdr_certificate)
       pk12 = OpenSSL::PKCS12.new(Passbook.certificate, Passbook.password)
       flag = OpenSSL::PKCS7::BINARY | OpenSSL::PKCS7::DETACHED
-      sign = OpenSSL::PKCS7.sign(pk12.certificate, pk12.key, content, [wwdr], flag)
-      data = OpenSSL::PKCS7.write_smime(sign)
-      Base64.decode64 extract_smime(data)
-    end
-    
-    def extract_smime(data)
-      start, finish = %[filename="smime.p7s"\n\n], %[\n\n------]
-      data[(data.index(start) + start.length)...(data.rindex(finish) + finish.length)]
+      OpenSSL::PKCS7.sign(pk12.certificate, pk12.key, content, [wwdr], flag).to_der
     end
   end
 end
